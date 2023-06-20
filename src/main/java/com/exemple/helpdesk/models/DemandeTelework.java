7a8b04ac55d9aeaa60ccf3293d4b227f363e6828
said147 <110072505+said147@users.noreply.github.com>
2023-06-20 12:44:02 +0100

    first

---
+++ src/main/java/com/exemple/helpdesk/models/DemandeTelework.java
@@ -0,0 +1,38 @@
+package com.exemple.helpdesk.models;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DemandeTelework implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_demandeTelework;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date date;
+    private String adresse ;
+    private String status  ;
+    private String Type_Demande;
+    private String DataURLEmploye  ;
+    private String DataURLCheker  ;
+    @OneToMany(targetEntity = MaterielTelework.class,cascade = CascadeType.ALL)
+    @JoinColumn(name = "idmt",referencedColumnName = "id_demandeTelework")
+
+    private List<MaterielTelework> materielTelework;
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
